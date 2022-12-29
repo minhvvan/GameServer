cede96bd7cb93914adf6c8f43bd79a2373536850
hvvan <eminhwan12@gmail.com>
2022-12-29 15:54:20 +0900

    SpinLock

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,8 +1,32 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include "CorePch.h"
+#include <thread>
+#include <atomic>
+#include <mutex>
+
+class SpinLock {
+public:
+	void lock() 
+	{
+		bool expected = false;
+		bool desired = true;
+
+		while (_locked.compare_exchange_strong(expected, desired) == false) {
+			expected = false;
+		}
+	}
+
+	void unlock()
+	{
+		_locked.store(false);
+	}
+
+private:
+	atomic<bool> _locked = false;
+};
 
 int main()
 {
-	HelloWorld();
+
 }
