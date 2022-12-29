047568e520ace4944d8f24716ff3306dcbd890ad
hvvan <eminhwan12@gmail.com>
2022-12-29 16:37:43 +0900

    Event

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -4,6 +4,7 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include <windows.h>
 
 class SpinLock {
 public:
@@ -32,7 +33,50 @@ private:
 	atomic<bool> _locked = false;
 };
 
+mutex m;
+queue<int32> q;
+HANDLE handle;
+
+
+void Producer()
+{
+	while (true)
+	{
+		{
+			unique_lock<mutex> lock(m);
+			q.push(100);
+		}
+		::SetEvent(handle);
+
+		this_thread::sleep_for(10000ms);
+	}
+}
+
+void Consumer()
+{
+	while (true)
+	{
+		//Manual Reset (false) --> 자동으로 signal이 꺼짐
+		::WaitForSingleObject(handle, INFINITE);
+
+		unique_lock<mutex> lock(m);
+		if(q.empty() == false){
+			int32 data = q.front();
+			q.pop();
+			cout << data << "\n";
+		}
+	}
+}
+
 int main()
 {
+	handle = ::CreateEvent(NULL/*보안속성*/, FALSE/*ManualReset*/, FALSE/*bInitalState*/, NULL);
 
+	thread t1(Producer);
+	thread t2(Consumer);
+
+	t1.join();
+	t2.join();
+
+	::CloseHandle(handle);
 }
