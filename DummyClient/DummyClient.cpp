8af10851ea529696066e43c6b1d58945d27ddc81
hvvan <eminhwan12@gmail.com>
2023-01-27 15:14:14 +0900

    TCP Server

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -8,16 +8,10 @@
 
 int main()
 {
-	// 윈속 초기화 (ws2_32 라이브러리 초기화)
-	// 관련 정보가 wsaData에 채워짐
 	WSAData wsaData;
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		return 0;
 
-	// ad : Address Family (AF_INET = IPv4, AF_INET6 = IPv6)
-	// type : TCP(SOCK_STREAM) vs UDP(SOCK_DGRAM)
-	// protocol : 0
-	// return : descriptor
 	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
 	if (clientSocket == INVALID_SOCKET)
 	{
@@ -26,18 +20,11 @@ int main()
 		return 0;
 	}
 
-	// 연결할 목적지는? (IP주소 + Port) -> XX 아파트 YY 호
 	SOCKADDR_IN serverAddr; // IPv4
 	::memset(&serverAddr, 0, sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
-	//serverAddr.sin_addr.s_addr = ::inet_addr("127.0.0.1"); << deprecated
 	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
 	serverAddr.sin_port = ::htons(7777); // 80 : HTTP
-	// host to network short
-	// Little-Endian vs Big-Endian
-	// ex) 0x12345678 4바이트 정수
-	// low [0x78][0x56][0x34][0x12] high < little
-	// low [0x12][0x34][0x56][0x78] high < big = network
 
 	if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
 	{
@@ -54,6 +41,34 @@ int main()
 	while (true)
 	{
 		// TODO
+		char sendBuffer[100] = "Hello World!";
+
+		for (int32 i = 0; i < 10; i++)
+		{
+			int32 resultCode = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
+			if (resultCode == SOCKET_ERROR)
+			{
+				int32 errCode = ::WSAGetLastError();
+				cout << "Send ErrorCode : " << errCode << endl;
+				return 0;
+			}
+		}
+
+
+		cout << "Send Data! Len = " << sizeof(sendBuffer) << endl;
+
+		/*char recvBuffer[1000];
+
+		int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+		if (recvLen <= 0)
+		{
+			int32 errCode = ::WSAGetLastError();
+			cout << "Recv ErrorCode : " << errCode << endl;
+			return 0;
+		}
+
+		cout << "Recv Data! Data = " << recvBuffer << endl;
+		cout << "Recv Data! Len = " << recvLen << endl;*/
 
 		this_thread::sleep_for(1s);
 	}
