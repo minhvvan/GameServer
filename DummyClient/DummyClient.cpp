d449bab33076a60dc1354a003d2fb7b97a6ddf08
hvvan <eminhwan12@gmail.com>
2023-01-27 15:43:52 +0900

    UDP Server

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -6,69 +6,78 @@
 #include <ws2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 
+void HandleError(const char* cause)
+{
+	int32 errCode = ::WSAGetLastError();
+	cout << cause << " ErrorCode : " << errCode << endl;
+}
+
+
 int main()
 {
 	WSAData wsaData;
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		return 0;
 
-	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
 	if (clientSocket == INVALID_SOCKET)
 	{
-		int32 errCode = ::WSAGetLastError();
-		cout << "Socket ErrorCode : " << errCode << endl;
+		HandleError("Socket");
 		return 0;
 	}
 
-	SOCKADDR_IN serverAddr; // IPv4
+	SOCKADDR_IN serverAddr;
 	::memset(&serverAddr, 0, sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
 	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
-	serverAddr.sin_port = ::htons(7777); // 80 : HTTP
+	serverAddr.sin_port = ::htons(7777);
 
-	if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
-	{
-		int32 errCode = ::WSAGetLastError();
-		cout << "Connect ErrorCode : " << errCode << endl;
-		return 0;
-	}
+	// Connected UDP
+	::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
 
 	// ---------------------------
-	// 연결 성공! 이제부터 데이터 송수신 가능!
-
-	cout << "Connected To Server!" << endl;
 
 	while (true)
 	{
-		// TODO
 		char sendBuffer[100] = "Hello World!";
 
-		for (int32 i = 0; i < 10; i++)
-		{
-			int32 resultCode = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
-			if (resultCode == SOCKET_ERROR)
-			{
-				int32 errCode = ::WSAGetLastError();
-				cout << "Send ErrorCode : " << errCode << endl;
-				return 0;
-			}
-		}
+		// 나의 IP 주소 + 포트 번호 설정
 
+		// Unconnected UDP
+		/*int32 resultCode = ::sendto(clientSocket, sendBuffer, sizeof(sendBuffer), 0,
+			(SOCKADDR*)&serverAddr, sizeof(serverAddr));*/
+
+			// Connected UDP
+		int32 resultCode = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
+
+		if (resultCode == SOCKET_ERROR)
+		{
+			HandleError("SendTo");
+			return 0;
+		}
 
 		cout << "Send Data! Len = " << sizeof(sendBuffer) << endl;
 
-		/*char recvBuffer[1000];
+		SOCKADDR_IN recvAddr;
+		::memset(&recvAddr, 0, sizeof(recvAddr));
+		int32 addrLen = sizeof(recvAddr);
+
+		char recvBuffer[1000];
+
+		// Unconnected UDP
+		//int32 recvLen = ::recvfrom(clientSocket, recvBuffer, sizeof(recvBuffer), 0,
+		//	(SOCKADDR*)&recvAddr, &addrLen);
 
 		int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+
 		if (recvLen <= 0)
 		{
-			int32 errCode = ::WSAGetLastError();
-			cout << "Recv ErrorCode : " << errCode << endl;
+			HandleError("RecvFrom");
 			return 0;
 		}
 
 		cout << "Recv Data! Data = " << recvBuffer << endl;
-		cout << "Recv Data! Len = " << recvLen << endl;*/
+		cout << "Recv Data! Len = " << recvLen << endl;
 
 		this_thread::sleep_for(1s);
 	}
