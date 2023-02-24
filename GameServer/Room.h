c9a2268d3e5d4a2cebc9ec6bb785a0e677c41eb0
hvvan <eminhwan12@gmail.com>
2023-02-24 14:23:57 +0900

    Job Queue#4

--- GameServer/Room.h
+++ GameServer/Room.h
@@ -1,7 +1,7 @@
 #pragma once
-#include "JobSerializer.h"
+#include "JobQueue.h"
 
-class Room : public JobSerializer
+class Room : public JobQueue
 {
 public:
 	// 싱글쓰레드 환경인마냥 코딩
@@ -9,10 +9,6 @@ public:
 	void Leave(PlayerRef player);
 	void Broadcast(SendBufferRef sendBuffer);
 
-public:
-	// 멀티쓰레드 환경에서는 일감으로 접근
-	virtual void FlushJob() override;
-	 
 private:
 	map<uint64, PlayerRef> _players;
 };
